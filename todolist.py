7ec7c0d86e178356b0fc682f03613e69af2e54d7
sunyanguo <sunyanguo@lvmama.com>
2017-05-03 19:41:30 +0800

    clean code

--- todolist.py
+++ todolist.py
@@ -2,11 +2,8 @@
 import sys
 import requests
 from bs4 import BeautifulSoup
-from datetime import datetime
 import json
 
-# print "Fetching Todo status"
-# print datetime.now().strftime("%Y/%m/%d %H:%M:%S (local time)")
 url="http://lvioscode.com/dashboard/todos?state=&utf8=✓&project_id=&author_id=&type=&action_id="
 cookies={}
 cookies["_gitlab_session"]="b164db7ed4344f78f46bc3a99d00a8b8"
@@ -17,22 +14,20 @@ html_doc = resp.text
 soup = BeautifulSoup(html_doc,"html.parser")
 # print soup
 allTodo = []
-for oneDiv in soup.find_all("div"):
-  panelClass = oneDiv["class"][0]
+for panelDiv in soup.find_all("div"):
+  panelClass = panelDiv["class"][0]
   if panelClass == "panel":
-    for rowDiv in oneDiv.find_all("div"):
-      panelHeadingClass = rowDiv["class"][0]
+    for panelHeadingDiv in panelDiv.find_all("div"):
+      panelHeadingClass = panelHeadingDiv["class"][0]
       if panelHeadingClass == "panel-heading":
         oneTodoList = []
-        for todoItemDiv in oneDiv.find_all("div"):
+        for todoItemDiv in panelDiv.find_all("div"):
           todoItemClass = todoItemDiv["class"][0]
           oneTodo = {}
           if todoItemClass == "todo-item":
-            oneTodo["space"] = (rowDiv.find_all("a")[0]).text.replace("\n", "").replace(" ", "")
+            oneTodo["space"] = (panelHeadingDiv.find_all("a")[0]).text.replace("\n", "").replace(" ", "")
             indexOfA = 0
             for title in todoItemDiv.find_all("a"):
-              # todo-item
-              # print rowDiv["class"][0]
               if indexOfA ==  0:
                 oneTodo["name"] = title.text.replace("\n", "").replace(" ", "")
               if indexOfA ==  1:
@@ -42,13 +37,6 @@ for oneDiv in soup.find_all("div"):
                 oneTodoList.append(oneTodo)
                 oneTodo = {}
               indexOfA = indexOfA + 1
-              # print title.text
-              print indexOfA
-          
-        # print oneTodo
     allTodo.append(oneTodoList)        
-          # print title.text
-
-# print allTodo
 jsondump = json.dumps(allTodo)
 print jsondump
